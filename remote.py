51e127873a33c660c57235ee3cdc733827a17271
Tyler <tyritchie14@gmail.com>
2025-03-06 20:37:30 -0700

    add to remote

--- remote.py
+++ remote.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 @app.route('/run-python', methods=['GET'])
 def run_script():
     # Your Python script logic here
-    print("Hello World")
+    file1 = open("test.txt", "w")
+    file1.write("Hello World")
+    file1.close
+
     return "Hello World"
 
 if __name__ == '__main__':
+    # run_script()
     app.run(debug=True)
